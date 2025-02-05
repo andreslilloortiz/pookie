00c164136ae933b8dd0763fe3f2210b8d8bfe239
andreslilloortiz <154079318+andreslilloortiz@users.noreply.github.com>
2025-02-05 22:20:12 +0100

    add: msvc in wine on linux example

---
+++ examples/MSVC_in_Wine_on_Linux_Example/MSVC_in_Wine_on_Linux_Example.md
@@ -0,0 +1,14 @@
+# Example of Setting Up and Running MSVC in Wine on Linux
+
+## Download the repository
+```bash
+git clone https://github.com/mstorsjo/msvc-wine.git
+```
+
+## Install
+```bash
+cd msvc-wine/
+./vsdownload.py --dest .
+./install.sh .
+```
+
--- python_compile/python_compile_macos_x86_64.sh
+++ python_compile/python_compile_macos_x86_64.sh
@@ -1,2 +1,15 @@
+wget https://www.python.org/ftp/python/3.13.1/Python-3.13.1.tar.xz >> /dev/null 2>> /dev/null
+
+tar -xvf Python-3.13.1.tar.xz
+
+cd Python-3.13.1/
+
+darling shell
+
 xcode-select --install
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install zlib bzip2 sqlite readline openssl xz gdbm
+
 
