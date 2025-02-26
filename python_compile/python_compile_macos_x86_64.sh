5431f0ef141314e299982ae68c49753e87193dbc
andreslilloortiz <154079318+andreslilloortiz@users.noreply.github.com>
2025-02-26 18:49:40 +0100

    mod: python compile macos x86_64, python test macos x86_64

--- python_compile/python_compile_macos_x86_64.sh
+++ python_compile/python_compile_macos_x86_64.sh
@@ -2,3 +2,29 @@
 
 # Compile Python 3.13.1 for MacOS x86_64
 echo ">> Compile Python 3.13.1 for MacOS x86_64"
+
+## Download Darling
+echo "	- Downloading Darling"
+wget https://github.com/darlinghq/darling/releases/download/v0.1.20230310_update_sources_11_5/darling_0.1.20230310.jammy_amd64.deb >> /dev/null 2>> /dev/null
+
+## Install Darling
+echo "	- Installing Darling"
+sudo dpkg -i darling_0.1.20230310.jammy_amd64.deb >> /dev/null 2>> /dev/null
+
+## Download Python
+echo "	- Downloading Python"
+wget https://www.python.org/ftp/python/3.13.1/python-3.13.1-macos11.pkg >> /dev/null 2>> /dev/null
+
+## Installing Python
+echo "	- Installing Python"
+darling shell <<EOF
+mkdir -p python_macos_x86_64
+installer -pkg python-3.13.1-macos11.pkg -target / >> /dev/null 2>> /dev/null
+cp -R /Library/Frameworks/Python.framework/Versions/3.13/* ./python_macos_x86_64/ >> /dev/null 2>> /dev/null
+EOF
+
+## Clean up temporary files
+rm darling_0.1.20230310.jammy_amd64.deb
+rm python-3.13.1-macos11.pkg
+
+echo ">> Python 3.13.1 installation in Darling completed"
--- python_test/python_test_macos_x86_64.sh
+++ python_test/python_test_macos_x86_64.sh
@@ -2,14 +2,3 @@
 
 # Test Python 3.13.1 for MacOS x86_64
 echo ">> Test Python 3.13.1 for MacOS x86_64"
-
-## Download Darling
-echo "	- Downloading Darling"
-wget https://github.com/darlinghq/darling/releases/download/v0.1.20230310_update_sources_11_5/darling_0.1.20230310.jammy_amd64.deb >> /dev/null 2>> /dev/null
-
-## Install Darling
-echo "	- Installing Darling"
-sudo dpkg -i darling_0.1.20230310.jammy_amd64.deb >> /dev/null 2>> /dev/null
-
-## Remove unnecessary files
-rm darling_0.1.20230310.jammy_amd64.deb
