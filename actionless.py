5bd25ba6905d1ea58be015d889bb4bb74ac42780
andreslilloortiz <154079318+andreslilloortiz@users.noreply.github.com>
2025-03-07 21:13:08 +0100

    mod: actionless.py

--- actionless.py
+++ actionless.py
@@ -13,13 +13,20 @@ def main():
 
     args = parser.parse_args()
 
+    # check args
+    for arg in vars(args):
+        print(f"{arg}: {getattr(args, arg)}")
+
+    # check if source file exists
     if not os.path.isfile(args.source):
         print(f"Error: {args.source} is not a valid file.")
         return
 
+    # iterate targets
     for target in args.target:
         if target == 'x86_64-linux':
 
+            print(">> Creating docker image for x86_64-linux")
             subprocess.run([
                 'docker', 'build', '-f', f'Dockerfile.{target}',
                 '--build-arg', f'PYTHON_VERSION={args.python_version}',
@@ -27,13 +34,17 @@ def main():
                 '-t', f'{target}', '.'
             ])
 
-            compiled = os.path.splitext(args.source)[0] + ".so"
-            subprocess.run([
-                'docker', 'run', '-it', '--rm',
-                '-v', f'{os.getcwd()}/workspace:/workspace',
-                f'{target}', '/bin/bash', '-c',
-                f'gcc -shared -o {compiled} -fPIC {args.source} $(python3-config --cflags --ldflags) && cp {compiled} /workspace'
-            ])
+            if (args.build == True or (args.build == False and args.test == False)):
+                print(">> Building the library for x86_64-linux")
+                compiled = os.path.splitext(args.source)[0] + ".so"
+                subprocess.run([
+                    'docker', 'run', '-it', '--rm',
+                    '-v', f'{os.getcwd()}/workspace:/workspace',
+                    f'{target}', '/bin/bash', '-c',
+                    f'gcc -shared -o {compiled} -fPIC {args.source} $(python3-config --cflags --ldflags) && cp {compiled} /workspace'
+                ])
+
+    print(">> Check the workspace directory for the compiled library")
 
 if __name__ == '__main__':
     main()
