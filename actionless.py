d37ff58990375af9ffccab7299d638ed5ffad905
andreslilloortiz <154079318+andreslilloortiz@users.noreply.github.com>
2025-03-07 21:04:34 +0100

    add: x86_64-linux docker commands in actionless.py

--- actionless.py
+++ actionless.py
@@ -1,35 +1,39 @@
 import argparse
 import os
+import subprocess
 
 def main():
     parser = argparse.ArgumentParser(description='Tool for Automating the Build and Testing Process of Native Python Libraries Using Cross-Compilation and Emulation Technologies')
 
-    parser.add_argument('package', type=str, help='Python package to build and test')
-    parser.add_argument('--build', action='store_true', help='Build the package')
-    parser.add_argument('--test', action='store_true', help='Test the package')
+    parser.add_argument('source', type=str, help='Python library to build and test')
+    parser.add_argument('--build', action='store_true', help='Build the library')
+    parser.add_argument('--test', action='store_true', help='Test the library')
     parser.add_argument('--python-version', type=str, default='3.13.1', help='Python version to use (default: 3.13.1)')
-    parser.add_argument('--target', type=str, required=True, nargs='+', choices=['x86_64-linux', 'x86_64-windows', 'x86_64-macos', 'arm64-macos'], help='Target platform(s) to build and test the package')
+    parser.add_argument('--target', type=str, required=True, nargs='+', choices=['x86_64-linux', 'x86_64-windows', 'x86_64-macos', 'arm64-macos'], help='Target platform(s) to build and test the library')
 
     args = parser.parse_args()
 
-    if not os.path.isfile(args.package):
-        print(f"Error: {args.package} is not a valid file.")
+    if not os.path.isfile(args.source):
+        print(f"Error: {args.source} is not a valid file.")
         return
 
-    print(args.package)
-    print(args.python_version)
-    if args.build:
-        print(f'build')
-    elif args.test:
-        print(f'test')
-    else:
-        print('build and test')
-
     for target in args.target:
-        print(target)
+        if target == 'x86_64-linux':
 
-# docker build -f Dockerfile.x86_64-linux --build-arg PYTHON_VERSION=3.13.1 --build-arg C_SOURCE_FILE=sum.c -t x86_64-linux .
-# docker run -it --rm x86_64-linux sh -c "gcc -shared -o sum.so -fPIC /sum.c \$(python3-config --cflags --ldflags)"
+            subprocess.run([
+                'docker', 'build', '-f', f'Dockerfile.{target}',
+                '--build-arg', f'PYTHON_VERSION={args.python_version}',
+                '--build-arg', f'C_SOURCE_FILE={args.source}',
+                '-t', f'{target}', '.'
+            ])
+
+            compiled = os.path.splitext(args.source)[0] + ".so"
+            subprocess.run([
+                'docker', 'run', '-it', '--rm',
+                '-v', f'{os.getcwd()}/workspace:/workspace',
+                f'{target}', '/bin/bash', '-c',
+                f'gcc -shared -o {compiled} -fPIC {args.source} $(python3-config --cflags --ldflags) && cp {compiled} /workspace'
+            ])
 
 if __name__ == '__main__':
     main()
