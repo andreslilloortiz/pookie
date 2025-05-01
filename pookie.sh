ccd8cf69a20b4c0489dc3d9982f40eed8cfe3315
andreslilloortiz <154079318+andreslilloortiz@users.noreply.github.com>
2025-05-01 09:48:14 +0200

    Reorganize and workspace flag

--- pookie.sh
+++ pookie.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-# Check if a directory argument was provided
-if [ -z "$1" ]; then
-    echo "Error: You must specify a workspace directory."
-    echo "Usage: $0 /path/to/workspace ARGUMENTS"
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --workspace)
+            WORKSPACE_DIR="$2"
+            shift 2
+            ;;
+        *)
+            ARGS+=("$1")
+            shift
+            ;;
+    esac
+done
+
+# Check if WORKSPACE_DIR is set
+if [ -z "$WORKSPACE_DIR" ]; then
+    echo "Error: You must specify a workspace directory using --workspace"
+    echo "Usage: $0 --workspace /path/to/workspace [OTHER_ARGUMENTS]"
     exit 1
 fi
 
-WORKSPACE_DIR="$1"
-
 echo ">> Creating pookie docker image"
 
-# Check if the Docker image 'win-macosx-pookie-lvl1-base' exists, if not, build it
+# Check if Docker images exist and build if necessary
 if ! docker image inspect win-macosx-pookie-lvl1-base > /dev/null 2>&1; then
     docker build -t win-macosx-pookie-lvl1-base -f images/win-macosx-pookie/Dockerfile.win-macosx-pookie-lvl1-base . > /dev/null 2>&1
     if [ $? -ne 0 ]; then
@@ -20,7 +32,6 @@ if ! docker image inspect win-macosx-pookie-lvl1-base > /dev/null 2>&1; then
     fi
 fi
 
-# Check if the Docker image 'win-macosx-pookie-lvl2-pookie' exists, if not, build it
 if ! docker image inspect win-macosx-pookie-lvl2-pookie > /dev/null 2>&1; then
     docker build -t win-macosx-pookie-lvl2-pookie -f images/win-macosx-pookie/Dockerfile.win-macosx-pookie-lvl2-pookie . > /dev/null 2>&1
     if [ $? -ne 0 ]; then
@@ -29,11 +40,11 @@ if ! docker image inspect win-macosx-pookie-lvl2-pookie > /dev/null 2>&1; then
     fi
 fi
 
-# Run the Docker container
+# Run Docker container with provided arguments
 docker run -it --rm \
     -v "$WORKSPACE_DIR":/workspace \
     -e WORKSPACE_PWD="$WORKSPACE_DIR" \
     -w /workspace \
     -v /var/run/docker.sock:/var/run/docker.sock \
     --network host \
-    win-macosx-pookie-lvl2-pookie "${@:2}"
+    win-macosx-pookie-lvl2-pookie "${ARGS[@]}"
