eb93dcb024b01ed6c653ec4d3d45d36392fe040e
andreslilloortiz <154079318+andreslilloortiz@users.noreply.github.com>
2025-05-01 11:16:13 +0200

    Wrapper python3 for win

--- src/docker_images_runner.py
+++ src/docker_images_runner.py
@@ -1,6 +1,48 @@
 import subprocess
 import os
 
+def rename_dist(original_dist_target, new_dist_target):
+    """
+    Generate the command to rename the built library files.
+    Place this command after the build command.
+
+    Parameters:
+    - original_target (str): The original target architecture.
+    - new_target (str): The new target architecture.
+
+    Returns:
+    - str: The command to rename the built library files.
+    """
+    return f' && for f in dist/*-{original_dist_target}.whl; do mv "$f" "${{f/{original_dist_target}/{new_dist_target}}}"; done'
+
+def install_dist(cp_version, dist_target):
+    """
+    Generate the command to install the built library.
+    Place this command before the test command.
+
+    Parameters:
+    - cp_version (str): The CP version to use in the filename.
+    - target (str): The target architecture.
+
+    Returns:
+    - str: The command to install the built library.
+    """
+    return f"python3 -m pip install dist/*-cp{cp_version}-cp{cp_version}*-{dist_target}.whl >> /dev/null 2>> /dev/null && "
+
+def wrapper_python3(new_python3_command):
+    """
+    Generate the command to create a wrapper for Python 3.
+    This is used to ensure that the correct Python interpreter is used in the Docker container.
+    Place this command before the build command.
+
+    Parameters:
+    - new_python3_command (str): The command to run the new Python 3 interpreter.
+
+    Returns:
+    - str: The command to create the wrapper.
+    """
+    return f'echo -e "#!/bin/bash\n{new_python3_command} \"\$@\"" > /bin/python3 && chmod +x /bin/python3 && '
+
 def run_lvl3_image(image_name, command, host_workspace_path, logfile):
     """
     Run a level 3 CP3xx Docker image.
@@ -50,11 +92,13 @@ def run_docker_images(targets, logfile, python_versions_dic, build, test, host_w
             if target == 'manylinux_2_17_x86_64':
 
                 image_name = f"manylinux-lvl3-cp{cp_version}-manylinux_2_17"
+                original_dist_target = "linux_x86_64"
+                new_dist_target = "manylinux_2_17_x86_64.manylinux2014_x86_64"
 
                 # build the library
                 if build != None:
 
-                    build_command = build + ' && for f in dist/*-linux_x86_64.whl; do mv "$f" "${f/linux_x86_64/manylinux_2_17_x86_64.manylinux2014_x86_64}"; done'
+                    build_command = build + rename_dist(original_dist_target, new_dist_target)
 
                     print(f">> Building the library for cp-{cp_version}-{target}")
                     run_lvl3_image(image_name, build_command, host_workspace_path, logfile)
@@ -62,7 +106,7 @@ def run_docker_images(targets, logfile, python_versions_dic, build, test, host_w
                 # test the library
                 if test != None:
 
-                    test_command = f"python3 -m pip install dist/*-cp{cp_version}-cp{cp_version}-manylinux_2_17_x86_64.manylinux2014_x86_64.whl >> /dev/null 2>> /dev/null && " + test
+                    test_command = install_dist(cp_version, new_dist_target) + test
 
                     print(f">> Testing the library for cp-{cp_version}-{target}")
                     run_lvl3_image(image_name, test_command, host_workspace_path, None)
@@ -71,11 +115,13 @@ def run_docker_images(targets, logfile, python_versions_dic, build, test, host_w
             if target == 'musllinux_1_2_x86_64':
 
                 image_name = f"musllinux-lvl3-cp{cp_version}-musllinux_1_2"
+                original_dist_target = "linux_x86_64"
+                new_dist_target = "musllinux_1_2_x86_64"
 
                 # build the library
                 if build != None:
 
-                    build_command = build + ' && for f in dist/*-linux_x86_64.whl; do mv "$f" "${f/linux_x86_64/musllinux_1_2_x86_64}"; done'
+                    build_command = build + rename_dist(original_dist_target, new_dist_target)
 
                     print(f">> Building the library for cp-{cp_version}-{target}")
                     run_lvl3_image(image_name, build_command, host_workspace_path, logfile)
@@ -83,7 +129,21 @@ def run_docker_images(targets, logfile, python_versions_dic, build, test, host_w
                 # test the library
                 if test != None:
 
-                    test_command = f"python3 -m pip install dist/*-cp{cp_version}-cp{cp_version}-musllinux_1_2_x86_64.whl >> /dev/null 2>> /dev/null && " + test
+                    test_command = install_dist(cp_version, new_dist_target) + test
 
                     print(f">> Testing the library for cp-{cp_version}-{target}")
                     run_lvl3_image(image_name, test_command, host_workspace_path, None)
+
+            # win_amd64
+            if target == 'win_amd64':
+
+                image_name = f"win-macosx-pookie-lvl3-cp{cp_version}-win"
+                new_python3_command = f"wine /python-{python_version}-embed-amd64/python.exe"
+
+                # build the library
+                if build != None:
+
+                    build_command = wrapper_python3(new_python3_command) + build
+
+                    print(f">> Building the library for cp-{cp_version}-{target}")
+                    run_lvl3_image(image_name, build_command, host_workspace_path, logfile)
